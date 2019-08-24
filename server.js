e43234d3e4133262541ee7f2c15a97eafccafe9e
Dcamacho <dantecamachoglz@gmail.com>
2019-08-24 13:30:41 -0500

    REST añadido

--- server.js
+++ server.js
@@ -1,24 +1,71 @@
 const express = require('express');
 var app = express();
+var bodyParser=require('body-parser')
+
+
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://admin:admin@cluster0-txmac.mongodb.net/test?retryWrites=true&w=majority";
+
 app.get('/', (req, res) => {
-    res.send('ALL OK');
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("rentame");
+        dbo.collection("users").find({}).toArray(function(err, result) {
+          if (err) throw err;
+          res.send(result)
+          db.close();
+        });
+      }); 
+    console.log("Consulta a usuario correcta");
 });
 
 app.post('/', (req, res) => {
-    res.send('ALL OK POST');
-    fakedb.push(req.body);
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("rentame");
+        dbo.collection("users").insertOne(req.body, function(err, res) {
+          if (err) throw err;
+          console.log("usuario insertado");
+          db.close();
+        });
+      });
+    res.send ("usuario insertado")
 });
 
 app.put('/', (req, res) => {
-    res.send('ALL OK PUT');
+    //res.send('ALL OK PUT');
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("rentame");
+        var myquery = {id:req.body.id}
+        var newvalues = { $set: {name: req.body.name, lastname: req.body.lastname } };
+        dbo.collection("users").updateOne(myquery, newvalues, function(err, res) {
+          if (err) throw err;
+          console.log("usuario actualizado");
+          db.close();
+        });
+    });
+    res.send ("usuario actualizado")
 });
 
 app.delete('/', (req, res) => {
-    res.send('ALL OK DELETE');
+    console.log(req.body)
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("rentame");
+        var myquery = {id:req.body.id}
+        dbo.collection("users").deleteOne(myquery, function(err, obj) {
+          if (err) throw err;
+          console.log("Usuario eliminado");
+          db.close();
+        });
+      });
+      res.send ("Usuario eliminado")
 });
 
 app.listen(3000,() => {
